f0085ba3507003d686d3a61fdba893251a8d1531
Mehmet Zeki Kara <karamehmetzeki506@gmail.com>
2020-05-25 17:40:21 +0300

    Fixing Bug

--- js/custom.js
+++ js/custom.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
 
 });
-// Fatih Postacı
+// // Fatih Postacı
 function readURLposta(input) {
 	if (input.files && input.files[0]) {
 		var reader = new FileReader();
@@ -122,6 +122,8 @@ $('#projeCalistirTurist').on('click',function(){
 })
 
 //zeki metin ekleme******************************
+
+
 
 //müze Container
 var dizimuzeText=[];
@@ -239,8 +241,168 @@ $('#btnEkle6').on('click', function () {
 });
 
 
-//Fotoğraf yükleme
+//Zeki chatbot
+//links
+//http://eloquentjavascript.net/09_regexp.html
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+
+var messages = [], //array that hold the record of each string in chat
+  lastUserMessage = "", //keeps track of the most recent input string from the user
+  botMessage = "", //var keeps track of what the chatbot is going to say
+  botName = 'Murtaza', //name of the chatbot
+  talking = true; //when false the speach function doesn't work
+//
+
+//edit this function to change what the chatbot says
+function chatbotResponse() {
+  talking = true;
+  botMessage = "I'm confused"; //the default message
+
+
+  for(i=0;i<=dizimuzeText.length;i++){
+	  if(lastUserMessage==dizimuzeText[i]){
+		  botMessage="Selam Bebek Mıgo Ben Kelebek";
+	  }
+	  
+   if (lastUserMessage === 'name') {
+     botMessage = 'My name is ' + botName;
+   }
+  }
+
+  for(i=0;i<=dizibalıkText.length;i++){
+	if(lastUserMessage==dizibalıkText[i]){
+		botMessage="Hamsi Kafalu Ne Yabucaun";
+	}
+	
+ if (lastUserMessage === 'name') {
+   botMessage = 'My name is ' + botName;
+ }
+}
+
+for(i=0;i<=dizidenizText.length;i++){
+	if(lastUserMessage==dizidenizText[i]){
+		botMessage="Yüzmen Lazım";
+	}
+	
+ if (lastUserMessage === 'name') {
+   botMessage = 'My name is ' + botName;
+ }
+}
+
+for(i=0;i<=diziyemekText.length;i++){
+	if(lastUserMessage==diziyemekText[i]){
+		botMessage="AÇ Mısın";
+	}
+	
+ if (lastUserMessage === 'name') {
+   botMessage = 'My name is ' + botName;
+ }
+}
+
+for(i=0;i<=diziotelText.length;i++){
+	if(lastUserMessage==diziotelText[i]){
+		botMessage="Otel Mi TRIVAGO";
+	}
+	
+ if (lastUserMessage === 'name') {
+   botMessage = 'My name is ' + botName;
+ }
+}
+for(i=0;i<=dizidogaText.length;i++){
+	if(lastUserMessage==dizidogaText[i]){
+		botMessage="Ağrı DağınE	Eteğinde ";
+	}
+	
+ if (lastUserMessage === 'name') {
+   botMessage = 'My name is ' + botName;
+ }
+}
+
+
+//   if (lastUserMessage === 'hi' || lastUserMessage =='hello') {
+//     const hi = ['hi','howdy','hello']
+//     botMessage = hi[Math.floor(Math.random()*(hi.length))];;
+//   }
+
+//   if (lastUserMessage === 'name') {
+//     botMessage = 'My name is ' + botName;
+//   }
+
+}
+
+//
+//this runs each time enter is pressed.
+//It controls the overall input and output
+function newEntry() {
+  //if the message from the user isn't empty then run 
+  if (document.getElementById("chatbox").value != "") {
+    //pulls the value from the chatbox ands sets it to lastUserMessage
+    lastUserMessage = document.getElementById("chatbox").value;
+    //sets the chat box to be clear
+    document.getElementById("chatbox").value = "";
+    //adds the value of the chatbox to the array messages
+    messages.push(lastUserMessage);
+    //Speech(lastUserMessage);  //says what the user typed outloud
+    //sets the variable botMessage in response to lastUserMessage
+    chatbotResponse();
+    //add the chatbot's name and message to the array messages
+    messages.push("<b>" + botName + ":</b> " + botMessage);
+    // says the message using the text to speech function written below
+    Speech(botMessage);
+    //outputs the last few array elements of messages to html
+    for (var i = 1; i < 8; i++) {
+      if (messages[messages.length - i])
+        document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
+    }
+  }
+}
+
+//text to Speech
+//https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
+function Speech(say) {
+  if ('speechSynthesis' in window && talking) {
+    var utterance = new SpeechSynthesisUtterance(say);
+    //msg.voice = voices[10]; // Note: some voices don't support altering params
+    //msg.voiceURI = 'native';
+    //utterance.volume = 1; // 0 to 1
+    //utterance.rate = 0.1; // 0.1 to 10
+    //utterance.pitch = 1; //0 to 2
+    //utterance.text = 'Hello World';
+    //utterance.lang = 'en-US';
+    speechSynthesis.speak(utterance);
+  }
+}
+
+//runs the keypress() function when a key is pressed
+document.onkeypress = keyPress;
+//if the key pressed is 'enter' runs the function newEntry()
+function keyPress(e) {
+  var x = e || window.event;
+  var key = (x.keyCode || x.which);
+  if (key == 13 || key == 3) {
+    //runs this function when enter is pressed
+    newEntry();
+  }
+  if (key == 38) {
+    console.log('hi')
+      //document.getElementById("chatbox").value = lastUserMessage;
+  }
+}
+
+//clears the placeholder text ion the chatbox
+//this function is set to run when the users brings focus to the chatbox, by clicking on it
+function placeHolder() {
+  document.getElementById("chatbox").placeholder = "";
+}
+
+
+
+
+
+
+
+//Fotoğraf yükleme
 function readURL(input) {
 	if (input.files && input.files[0]) {
 		var reader = new FileReader();
